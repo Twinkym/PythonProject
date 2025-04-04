fa19a020c2c611a154ce0db28b1efb8653d516df
david de la puente enriquez <Twinkymen@hotmail.com>
2025-04-04 23:42:23 +0200

    Segundo proyecto Python correspondiente al segundo temario del curso

---
+++ Operadores/operadores_aritmeticos.py
@@ -0,0 +1,32 @@
+# Ejemplo de operadores aritmeticos.
+# a = 30
+# b = 3
+a, b = 10, 3
+
+# suma +
+suma = a + b
+print(f'suma: {suma}')
+
+# Resta -
+resta = a - b
+print(f'Resta: {resta}')
+
+# Multiplicación *
+multiplicacion = a * b
+print(f'Multiplicación: {multiplicacion}')
+
+# División /
+division = a / b
+print(f'División: {division}')
+
+# División entera //
+division_Exacta = a // b
+print(f'Division Exacta: {division_Exacta}')
+
+# Resto (resto de la division) %
+resto = a % b
+print(f'Resto: {resto}')
+
+# Potencia **
+potencia = a ** b  # 10 elevado a la 3 -> 10*10*10
+print(f'Potencia: {potencia}')
